862c8e03c280f9a2f52d8737cdd01339f4b440f8
Javier Lopez <jlopez@ontrajectory.com>
2022-09-06 15:13:19 -0500

    diccionarios con python

--- diccionarios.py
+++ diccionarios.py
@@ -1,11 +1,23 @@
-diccionario={}
+diccionario={
 
-diccionario['nombre']=input("Digite su nombre: ")
+    "nombre" : "juan",
+    "edad" : 30,
+    "posicion" : "delantero",
+    "convocado" : True
+}
 
-print(diccionario)
-print(diccionario['nombre'])
+# diccionario['nombre']=input("Digite su nombre: ")
+
+# print(diccionario)
+# print(diccionario['nombre'])
 
 
-for llave,valor in diccionario.items():
-    print(llave)
+# for llave,valor in diccionario.items():
+#     print(llave)
+#     print(valor)
+
+for valor in diccionario.values():
     print(valor)
+
+# llave es nombre
+#valor es juan
